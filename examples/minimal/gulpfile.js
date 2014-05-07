ce8e907345e10e18e41c87b1b7825afeab33bf82
Thomas Vervest <thomas@squarewolf.nl>
2014-05-07 08:44:31 +0200

    Added minimal gulp-neuter example, as requested in issue #3

---
+++ examples/minimal/gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var neuter = require('../../');
+var clean = require('gulp-clean');
+
+var inputDir = 'src';
+var buildDir = 'build';
+
+var config = {
+	js: {
+		input: inputDir + '/main.js',
+		output: buildDir,
+	},
+	html: {
+		input: [
+			inputDir + '/**/*.html',
+			inputDir + '/**/*.htm',
+		],
+		output: buildDir,
+	}
+};
+
+gulp.task('clean', function() {
+	var paths = [
+		buildDir + '/**/*',
+	];
+
+	return gulp.src(paths, {
+			read: false
+		})
+		.pipe(clean());
+});
+
+gulp.task('build-html', function() {
+	return gulp.src(config.html.input)
+		.pipe(gulp.dest(config.html.output));
+});
+
+gulp.task('build-js', function() {
+	return gulp.src(config.js.input)
+		.pipe(neuter('main.js', 'main.map', {
+			filepathTransform: function (filepath) {
+				return inputDir + '/' + filepath;
+			},
+		}))
+		.pipe(gulp.dest(config.js.output));
+});
+
+gulp.task('default', [
+	'build-html',
+	'build-js',
+	'clean',
+], function() {
+});
